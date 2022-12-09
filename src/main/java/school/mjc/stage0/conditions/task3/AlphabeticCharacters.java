c3402f9efa434cd6da005fd7ef79ec71c23b839a
AlekseiKasperovich <AleksKasperovich@yandex.by>
2022-12-09 15:49:11 +0300

    commit

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -1,6 +1,27 @@
 package school.mjc.stage0.conditions.task3;
 
+import java.util.Locale;
+
 public class AlphabeticCharacters {
+
     public void vowelDeterminer(char character) {
+        if ((character >= 65 && character <= 90) || (character >= 97 && character <= 122)) {
+            String symbol = String.valueOf(character).toUpperCase(Locale.ROOT);
+            char[] upperCaseSymbol = symbol.toCharArray();
+            char[] vowel = {'A', 'E', 'I', 'O', 'U', 'Y'};
+            boolean flag = true;
+            for (int i = 0; i < vowel.length; i++) {
+                if (upperCaseSymbol[0] == vowel[i]) {
+                    flag = false;
+                    System.out.println("Vowel");
+                    break;
+                }
+            }
+            if (flag) {
+                System.out.println("Consonant");
+            }
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -1,6 +1,23 @@
 package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
+
     public void calculateSalary(int salary) {
+        double res;
+        if (salary > 0 && salary <= 10000) {
+            res = salary - salary * 0.15;
+            System.out.println(res);
+        }
+        if (salary > 10000 && salary <= 20000) {
+            res = salary - salary * 0.18;
+            System.out.println(res);
+        }
+        if (salary > 20000) {
+            res = salary - salary * 0.20;
+            System.out.println(res);
+        }
+        if (salary <= 0) {
+            System.out.println("wrong input!");
+        }
     }
 }
